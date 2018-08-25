6b4cd95401d1855783f9b6afd598124a94358b66
eldadru <eldadru@luminate.io>
2018-08-25 17:31:05 +0300

    Adding namespace flag to allow users specify namespace other than default

--- ksniff.sh
+++ ksniff.sh
@@ -2,11 +2,13 @@
 
 POD_NAME=${KUBECTL_PLUGINS_LOCAL_FLAG_POD:-$1}
 CONTAINER_NAME=${KUBECTL_PLUGINS_LOCAL_FLAG_CONTAINER}
+NAMESPACE_NAME=${KUBECTL_PLUGINS_LOCAL_FLAG_NAMESPACE}
 FILTER=${KUBECTL_PLUGINS_LOCAL_FLAG_FILTER}
 CONTAINER_FLAG=""
+NAMESPACE_FLAG=""
 
 function usage() {
-  echo "[+] Usage: ./ksniff.sh <POD_NAME> [-c <CONTAINER_NAME>] [-f <CAPTURE FILTER>]"
+  echo "[+] Usage: ./ksniff.sh <POD_NAME> [-n <NAMESPACE_NAME>] [-c <CONTAINER_NAME>] [-f <CAPTURE FILTER>]"
 	exit 1
 }
 
@@ -18,17 +20,28 @@ if [ -n "$CONTAINER_NAME" ]; then
   CONTAINER_FLAG="-c ${CONTAINER_NAME}"
 fi
 
-echo "[+] Sniffing on ${POD_NAME}"
+if [ -n "$NAMESPACE_NAME" ]; then
+  NAMESPACE_FLAG="-n ${NAMESPACE_NAME}"
+fi
+
+echo "[+] Sniffing on pod: ${POD_NAME} container: ${CONTAINER_NAME} namespace: ${NAMESPACE_NAME}"
+
+echo "[+] Verifying pod status"
+kubectl get pod ${POD_NAME} ${NAMESPACE_FLAG}
+if [[ $? -ne 0 ]]; then
+  echo "[-] Pod is not existing or on different namespace"
+  exit 1
+fi
 
 echo "[+] checking if tcpdump already exist"
-kubectl exec ${POD_NAME} ${CONTAINER_FLAG} -- ls -alt /static-tcpdump
+kubectl exec ${POD_NAME} ${CONTAINER_FLAG} ${NAMESPACE_FLAG} -- ls -alt /static-tcpdump
 if [[ $? -ne 0 ]]; then
 	echo "[+] couldn't find static tcpdump binary, uploading static tcpdump to container"
-	kubectl cp ./static-tcpdump ${POD_NAME}:/static-tcpdump ${CONTAINER_FLAG}
-	kubectl exec ${POD_NAME} ${CONTAINER_FLAG} -- chmod +x /static-tcpdump
+	kubectl cp ./static-tcpdump ${POD_NAME}:/static-tcpdump ${CONTAINER_FLAG} ${NAMESPACE_FLAG}
+	kubectl exec ${POD_NAME} ${CONTAINER_FLAG} ${NAMESPACE_FLAG} -- chmod +x /static-tcpdump
 else
 	echo "[+] static tcpdump is already installed on container!"
 fi
 
 echo "[+] Starting remote sniffing!"
-kubectl exec ${POD_NAME} ${CONTAINER_FLAG} -- /static-tcpdump -s0 -w - ${FILTER} | wireshark -k -i -
+kubectl exec ${POD_NAME} ${CONTAINER_FLAG} ${NAMESPACE_FLAG} -- /static-tcpdump -s0 -w - ${FILTER} | wireshark -k -i -
