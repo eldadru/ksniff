fe008940573f17f3c274c02ef2ed19725df1f4cd
eldadru <eldadru@luminate.io>
2019-01-11 13:32:08 +0200

    importing more clients auth to increase support

--- pkg/cmd/sniff.go
+++ pkg/cmd/sniff.go
@@ -19,6 +19,10 @@ import (
 	"os/exec"
 	"path/filepath"
 	"time"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
 var (
