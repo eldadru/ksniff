d7feeed31e9c12166001f4736eafda17171ceaf5
Matt Long <mtl@wgtwo.com>
2021-01-20 14:36:24 +0100

    Add simple test for the docker cleanup command

--- pkg/service/sniffer/runtime/docker_test.go
+++ pkg/service/sniffer/runtime/docker_test.go
@@ -26,3 +26,17 @@ func TestPrivilegedPodName(t *testing.T) {
 	bridge.BuildTcpdumpCommand(&containerId, netInterface, filter, &pid, path)
 	assert.NotEqual(t, "", bridge.tcpdumpContainerName, "tcpdumpContainerName should have been set")
 }
+
+func TestCleanupCommand(t *testing.T) {
+	bridge := NewDockerBridge()
+	var containerId = "container"
+	var netInterface = "eth0"
+	var filter = "filter"
+	var pid = "pid"
+	var socketPath = "/path"
+	bridge.BuildTcpdumpCommand(&containerId, netInterface, filter, &pid, socketPath)
+	assert.Equal(t,
+		[]string{"docker", "--host", "unix://" + socketPath, "rm", "-f", bridge.tcpdumpContainerName},
+		bridge.BuildCleanupCommand(),
+		"container cleanup command doesn't match")
+}
